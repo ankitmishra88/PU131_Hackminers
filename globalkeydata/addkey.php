d683ddc5fa4cccdfe798a607e433ac6a082dfc40
Ridham Goyal <54924538+ridhamgoyal07@users.noreply.github.com>
2020-08-02 12:10:38 +0530

    Update addkey.php

--- globalkeydata/addkey.php
+++ globalkeydata/addkey.php
@@ -8,6 +8,12 @@
 	require "conn.inc.php";
 		
 	?>
+    <div>
+		<br />
+        
+		<a href="view-global.php">View keys</a>
+		<br />
+	</div>
 	<?php 
 		if(isset($_POST['submit'])){
 			$mykey=mysqli_real_escape_string($conn,$_POST['mykey']);
@@ -18,24 +24,19 @@
 
 			// echo "<script>window.location.reload(); exit();</script>";
 		}
-		mysqli_close($conn);
+		
 
 	?>
-	<div>
-		<br />
-		<a href="viewkey.php">View keys</a>
-		<br />
-	</div>
+	
 
 	
 
 	<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" >
 		<label for="mykey">Name Of Key</label>
 		<input type="text" name="mykey" id="mykey" placeholder="Name Of Global Key" required>
-		<label for="myvalue">Value</label>
+		<label for="myvalue">Cost</label>
 		<input type="number" name="myvalue" id="myvalue" placeholder="Value of Key" step="0.0001" required>
-		
-		<br />
+	
 		<input type="submit" name="submit" value="Save"> 
 	</form>
 </body>
