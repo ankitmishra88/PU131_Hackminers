eaf7b82a6f82a9e4ffffcd8445042b15570cf25f
Ridham Goyal <54924538+ridhamgoyal07@users.noreply.github.com>
2020-08-02 10:58:20 +0530

    Update vehicledata.php

--- vehiclemanagement/vehicledata.php
+++ vehiclemanagement/vehicledata.php
@@ -26,15 +26,15 @@
 			// Refresh Page without resubmit data
 			header("Refresh: 0");
 			// Redirect to the page to show page to show all vehicles
-			header("Location: view-vehicle.php");
-			// header("Location: viewvehicles.php");
+			// header("Location: view-vehicle.php");
+			header("Location: viewvehicles.php");
 		}
 		mysqli_close($conn);
 
 	?>
 	<div>
 		<br />
-		<a href="view-vehicle.php">View Vehicles</a>
+		<a href="viewvehicles.php">View Vehicles</a>
 		<br />
 	</div>
 
@@ -42,28 +42,28 @@
 		<label for="name">Name Of Truck</label>
 		<input type="text" name="name" id="name" placeholder="Name Of Truck" required>
 		<label for="capacity">Capacity</label>
-		<input type="number" name="capacity" id="capacity" placeholder="Capacity in tons" step="0.01" required>
+		<input type="number" name="capacity" id="capacity" placeholder="Capacity in tons" step="0.001" required>
 		<label for="distance">Distance</label>
-		<input type="number" name="distance" id="distance" placeholder="Distance in Km" step="0.1" required>
+		<input type="number" name="distance" id="distance" placeholder="Distance in Km" step="0.001" required>
 		<label for="mileage">Mileage</label>
-		<input type="number" name="mileage" id="mileage" placeholder="Mileage in Km" step="0.01" required>
+		<input type="number" name="mileage" id="mileage" placeholder="Mileage (Km/L)" step="0.001" required>
 		<label for="driverCost">Driver Cost</label>
-		<input type="number" name="driverCost" id="driverCost" placeholder="Driver Cost in Rupees" required>
-		<br />
+		<input type="number" name="driverCost" id="driverCost" placeholder="Driver Cost in Rupees (Cost/Km)" required>
+		<br /> 
 		<label for="documents">Documents and Insurance Cost</label>
-		<input type="number" name="documents" id="documents" placeholder="Documents and Insurance Cost" step="0.01" required>
+		<input type="number" name="documents" id="documents" placeholder="Documents and Insurance Cost (Cost/Km)" step="0.001" required>
 		<label for="emi">EMI</label>
-		<input type="number" name="emi" id="emi" placeholder="EMI in Rupees" step="0.01" required>
+		<input type="number" name="emi" id="emi" placeholder="EMI in Rupees (Cost/Km)" step="0.001" required>
 		<label for="tyre">Tyre</label>
 		<input type="number" name="tyre" id="tyre" placeholder="Tyres" required>
 		<label for="maintenance">Maintenance</label>
-		<input type="number" name="maintenance" id="maintenance" placeholder="Cost of Maintenance" step="0.01" required>
+		<input type="number" name="maintenance" id="maintenance" placeholder="Cost of Maintenance (Cost/Km)" step="0.001" required>
 		<label for="vehiclemode">Select mode of vehicle</label>
 		<select name="vehiclemode" id="vehiclemode">
 			<option value="LCV">LCV</option>
 			<option value="Truck">Truck</option>
 			<option value="Upto 3 Axle">Upto 3 Axle Vehicle</option>
-			<option value="4 to 6 Axle">4 to 6 Axle Vehicle</option>
+			<option value="4 to 6Axle">4 to 6 Axle Vehicle</option>
 			<option value="7 or more">7 or more Axle</option>
 			<option value="HCM/EME">HCM/EME</option>
 
