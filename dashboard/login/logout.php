0e68a612af2de76f10cb3482309e23b8c3323f71
Amitoj Singh Ahuja <singh99amitoj@gmail.com>
2020-08-12 21:00:55 +0530

    documented logout.php

--- dashboard/login/logout.php
+++ dashboard/login/logout.php
@@ -1,8 +1,11 @@
 <?php
+	// start session
    session_start();
+    // unset the session
    session_unset();
-
+   // destroy the session , if successfull ,, redirect to login.php file
    if(session_destroy()) {
+   		// redirecting
       header("Location: login.php");
    }
    exit();
