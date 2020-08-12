c075773bd7e36297bff7c46165156160c6c4627a
Amitoj Singh Ahuja <singh99amitoj@gmail.com>
2020-08-12 21:26:42 +0530

    documented

--- vehiclemanagement/vehicledelete.php
+++ vehiclemanagement/vehicledelete.php
@@ -1,17 +1,19 @@
-
-	<?php 
-	require "conn.inc.php";
-		
-	?>
+<!-- File for deleting record in database table  -->
+<?php 
+	// initilize the connection
+	require "conn.inc.php";		
+?>
 
 <?php
-
+	// getting id whose data to be deleted in database
 	$id = $_GET['id'];
-
+	// sql query for deleting the record where id = $id 
 	$sql = "DELETE FROM `vehicletable` WHERE 'Identity'='$id'";
-	mysqli_query($conn,"DELETE FROM `vehicletable` WHERE `vehicletable`.`Identity` = $id") or die(mysqli_error());
+	// run the query and check for the error 
+	// mysqli_query($conn,"DELETE FROM `vehicletable` WHERE `vehicletable`.`Identity` = $id") or die(mysqli_error());
 	mysqli_query($conn,$sql) or die(mysqli_error());
-	
+	// refreshing the page
 	header("Refresh: 0");
+	// redirecting to view-vehicle webpage to view all vehicles 
 	header("Location:/dashboard/view-vehicle.php");
 ?>
