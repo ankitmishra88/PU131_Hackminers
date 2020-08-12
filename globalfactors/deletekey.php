55067859e1c15f0b6daee811a3f263dd0bc8ac02
Amitoj Singh Ahuja <singh99amitoj@gmail.com>
2020-08-12 21:06:54 +0530

    documented

--- globalfactors/deletekey.php
+++ globalfactors/deletekey.php
@@ -1,16 +1,19 @@
-	<?php 
+<!--File for deleting a global key -->
+<?php 
+	// establishing connection with database 
 	require "conn.inc.php";
 		
-	?>
+?>
 
 <?php
-
+	// getting id of key whose record is to delete from database
 	$id = $_GET['id'];
-
+	// sql query to delete record where id is equal to value of $id varible
 	$sql = "DELETE FROM `globaltable` WHERE 'Identity'='$id'";
+	// running sql query
 	mysqli_query($conn,"DELETE FROM `globaltable` WHERE `globaltable`.`Identity` = $id") or die(mysqli_error());
 
-
+	// redirecting to view-globals.php file 
 	echo "<script>window.location.href='/dashboard/view-global.php';</script>";
 
 ?>
