d1dd1ab889754955b1c620e9c50ff8aed9d7eefb
Amitoj Singh Ahuja <singh99amitoj@gmail.com>
2020-08-12 21:21:05 +0530

    documented

--- vehiclemanagement/viewvehicles.php
+++ vehiclemanagement/viewvehicles.php
@@ -6,11 +6,11 @@
 
 </head>
 <body>
-
+	<!-- File for viewing all vehicles with some operation links like editing, deleting and viewing with each -->
 
 	<?php 
-	require "conn.inc.php";
-		
+	// extablish connection with database
+		require "conn.inc.php";	
 	?>
 
 	<div class="container mt-5">
@@ -24,28 +24,36 @@
 		<div class="allVehicles">
 
 			<?php 
+				// sql query fpr getting all vehicles record from 'vehicletable' and display their name and capacity and sort them by their name in desending order
 				$sql = "SELECT * FROM `vehicletable` ORDER BY 'NameOfTruck' DESC";
+				// running the sql query 
 				$query = mysqli_query($conn,$sql) or die(mysqli_error($conn));
+				// start of while loop
 				while($result = mysqli_fetch_assoc($query)){
 					?>
 					<div class="row ">
+						<!-- Display the name and capacity of each vehicle with edit , view , and delete operation buttons   -->
 						<div class="col-3"><?php echo '<b>'.$result['NameOfTruck'].'</b>'; ?></div>
 						<div class="col-3"><?php echo '<b>'.$result['Capacity'].'</b>'; ?></div> 
 						<div class="col-2">
+							<!-- redirecting the edit-vehicle.php page when click with the vehicle id specified in link for updating its data-->
 							<a href="edit-vehicle.php?id=<?php echo $result['Identity'];?>"><i class="far fa-edit"></i></a>
                            <!-- <a href="editvehicle.php?id=<?php echo $result['Identity'];?>">Edit</a>-->
 						</div>
 						<div class="col-1">
+							<!-- redirecting the vehicle-detail.php page when click with the vehicle id specified in link for viewing its complete detail-->
 							<a  href="vehicle-detail.php?id=<?php echo $result['Identity'];?>"><i class="far fa-eye"></i></a>
                             <!--<a href="vehicledetail.php?id=<?php echo $result['Identity'];?>">View</a>-->
 						</div>
 						<div class="col-1">
-							<a   href="../vehiclemanagement/vehicledelete.php?id=<?php echo $result['Identity'] ?>"><i class="fas fa-trash"></i></span></a>
+							<!-- redirecting the vehicledelete.php page when click with the vehicle id specified in link for deleting its record from database-->
+							<a href="../vehiclemanagement/vehicledelete.php?id=<?php echo $result['Identity'] ?>"><i class="fas fa-trash"></i></span></a>
                             <!--<a  href="vehicledelete.php?id=<?php echo $result['Identity'] ?>">Delete</span></a>-->
 						</div>
 					</div>
 
 			<?php
+				// end of while loops
 				}
 			?>
 		</div>
